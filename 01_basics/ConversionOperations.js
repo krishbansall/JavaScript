1aa225b912719ecb648f2bece3af802256a371e5
krishbansall <krishbansal9876@gmail.com>
2026-08-09 14:50:23 +0530

    Learning about conversion of operations

--- 01_basics/ConversionOperations.js
+++ 01_basics/ConversionOperations.js
@@ -1,44 +1,44 @@
-let score = "33abc"
+// let score = "33abc"
 
-console.log(typeof score); // String
-console.log(typeof (score));
+// console.log(typeof score); // String
+// console.log(typeof (score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber); // NaN (Not a number)
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN (Not a number)
 
-// ------------------------------------------------------------------------------------
+// // ------------------------------------------------------------------------------------
 
-let score = null
+// let score = null
 
-console.log(typeof score); // Object
-console.log(typeof (score));
+// console.log(typeof score); // Object
+// console.log(typeof (score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber); // 0
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // 0
 
-// ------------------------------------------------------------------------------------
+// // ------------------------------------------------------------------------------------
 
-let score = undefined
+// let score = undefined
 
-console.log(typeof score); // Undefined
-console.log(typeof (score));
+// console.log(typeof score); // Undefined
+// console.log(typeof (score));
 
-let valueInNumber = Number(score);
-console.log(typeof valueInNumber);
-console.log(valueInNumber); // NaN (Not a Number)
+// let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber); // NaN (Not a Number)
 
-// ------------------------------------------------------------------------------------
+// // ------------------------------------------------------------------------------------
 
-let score = 1
+// let score = 1
 
-console.log(typeof score);
-console.log(typeof (score));
+// console.log(typeof score);
+// console.log(typeof (score));
 
-let BolleanIsLoggedIn = Boolean(score);
-console.log(typeof BolleanIsLoggedIn);
-console.log(BolleanIsLoggedIn); 
+// let BolleanIsLoggedIn = Boolean(score);
+// console.log(typeof BolleanIsLoggedIn);
+// console.log(BolleanIsLoggedIn); 
 
 /* 
 1 => True
@@ -47,4 +47,27 @@ console.log(BolleanIsLoggedIn);
 "Krish" => True
 */
 
-// ------------------------------------------------------------------------------------
+// ----------------------------------------Operations----------------------------------------
+
+let value = 3
+let negvalue = -value
+
+console.log(value);
+console.log(negvalue);
+
+str1 = "Hello"
+str2 = " Krish"
+
+str3 = str1 + str2
+console.log(str3);
+
+console.log("1" + 2); // Output = 12
+console.log(1 + "2"); // Output = 12
+console.log("1" + "2"); // Output = 12
+console.log("1" + 2 + 2); // Output = 122
+console.log(1 + 2 + "2"); // Output = 32
+
+console.log(+true);
+console.log(+"");
+
+
