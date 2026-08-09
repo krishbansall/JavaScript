888af5d2079dc5306c3a1151f52f326f71653571
krishbansall <krishbansal9876@gmail.com>
2026-08-09 17:34:31 +0530

    Data Types Summary

--- 01_basics/DataTypes_Summary.js
+++ 01_basics/DataTypes_Summary.js
@@ -27,6 +27,32 @@ const MyFunction = function(){
     console.log("Hello World");
 }
 
+// ---------------------------------------------------------------------------------
+
+// Stack(Primitive) and Heap Memory(Non Primitive)
+
+let myChannelName = "Krish Bansal"
+
+let anotheChannelName = myChannelName
+
+anotheChannelName = "Chai aur Code"
+
+console.log(myChannelName);//Stack takes the copy of the memory address not tha actual address
+console.log(anotheChannelName);// Any changes done is done in the copy of the element
+
+
+let user1 = {
+    email : "krishbansal",
+    upi : "9876@ybl"
+}
+
+let user2 = {
+    email : "krishbnsl",
+    upi : "krishbansal@ybl"
+}
+user1 = user2
+console.log(user1.email);// This is stored in a object where change in memory address is done to the original address of the element
+console.log(user2.email);// This is a heap memory and a non primitive data type.
 
 
 
